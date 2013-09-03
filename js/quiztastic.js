3936b98df931bdd2d4b2c5dff542e602710cd23d
electronic Max <max@hip.cat>
2013-09-03 16:14:33 +0100

    bugfix on quiztastic

--- js/quiztastic.js
+++ js/quiztastic.js
@@ -1,35 +1,28 @@
 angular.module('quiztastic', ['ui'])
-	.directive('quiz', function() { 
+	.directive('quiz', function() {
 		return {
 			restrict :'E',
 			replace:'true',
-			templateUrl: "templates/quiz.html", 
+			templateUrl: "templates/quiz.html",
 			controller:function($scope, $attrs) {
 				var url = $attrs.data;
-				console.log('getting data from ', url);
 				var correct_answer = function(q,a) {
 					return q.options[q.answer] == a;
 				};
 				$scope.answer = function(q,a) {
-					console.log('got a question - ', q, 'answer', a);
 					q.response = a;
 					if (correct_answer(q,a)) {
-						console.log('answer is correct', a);
-						q.correct = true; 
+						q.correct = true;
 						q.explanation = a + " is correct!";
-					} 
-				};
-				$.get(url).then(function(data) {
-					try {
-						data = JSON.parse(data);
-						console.log('data is ', data);
-						$scope.$apply(function() {
-							$scope.questions = data.questions;
-						});
-					} catch(e) {
-						console.error('Malformed JSON in ' + url + ' - check your syntax and path and try again', e)
 					}
+				};
+				$.getJSON(url).then(function(data) {
+					$scope.$apply(function() {
+						$scope.questions = data.questions;
+					});
+				}).fail(function(err) {
+					console.error('Could not load ' + url + ' - check your syntax and path and try again', err);
 				});
 			}
-		};	
+		};
 	});
